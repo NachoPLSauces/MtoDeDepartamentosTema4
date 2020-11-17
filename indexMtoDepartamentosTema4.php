abecd41b50044e89245ef97942200d107d24c528
Nacho <Nacho@LAPTOP-00RLUILC>
2020-11-17 19:28:22 +0100

    Añadida estructura de carpetas

---
+++ indexMtoDepartamentosTema4.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="./webroot/css/estilo.css">
+
+        <title>NPL - Proyecto DAW2</title>
+    </head>
+
+    <body>
+        <header>
+            <h1>Mantenimiento de Departamentos</h1>
+        </header>
+        
+        <main>
+            <div class="topBar">
+                <div class="botonExportar">
+                    <p><a href="#">EXPORTAR</a></p>
+                </div>
+                <div class="botonImportar">
+                    <p><a href="#">IMPORTAR</a></p>
+                </div>
+                <div class="botonAdd">
+                    <p><a href="#">AÑADIR</a></p>
+                </div>
+            </div>
+            
+            <div class="buscarDepartamentos">
+                <div class="botonBuscar">
+                    <p><a href="#">BUSCAR</a></p>
+                </div>
+            </div>
+            
+            <div class="botBar">
+                <div class="botonVolver">
+                    <p><a href="../proyectoDWES/indexProyectoDWES.php">VOLVER</a></p>
+                </div>
+            </div>
+        </main>
+        
+        <footer>
+            <p>2020-2021 - Nacho del Prado Losada - ignacio.pralos@educa.jcyl.es</p>
+        </footer>
+    </body>
+</html>
