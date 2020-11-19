c3e9673ffbf328f9c893827c271a9fac5902b703
Nacho del Prado Losada <ignacio.pralos@educa.jcyl.es>
2020-11-19 12:18:23 +0100

    Actualización index Apariencia, buscar departamentos Librería de validación y confDBPDO

--- indexMtoDepartamentosTema4.php
+++ indexMtoDepartamentosTema4.php
@@ -9,6 +9,28 @@
     </head>
 
     <body>
+        <?php
+        /*
+         * @author: Nacho del Prado Losada
+         * @since: 19/11/2020
+         * @description: index de la aplicación Mantenimiento de Departamentos
+         */
+
+        //Llamada a la librería de validación de formularios
+        require_once 'core/201020libreriaValidacion.php';
+        //Llamada al fichero de almacenamiento de consantes en PDO
+        require_once 'config/confDBPDO.php';
+        
+        //Array de errores inicializado a null
+        $aErrores = ["DescDepartamento" => null];
+
+        //Varible de entrada correcta inicializada a true
+        $entradaOK = true;           
+
+        //Array de respuestas inicializado a null
+        $aRespuestas = ["DescDepartamento" => null];
+        ?>
+        
         <header>
             <h1>Mantenimiento de Departamentos</h1>
         </header>
@@ -23,33 +45,108 @@
             <div class="buscarDepartamentos">
                 <form name="input" action="<?php $_SERVER['PHP_SELF']?>" method="post">
                     <label>Descripción </label>
-                    <input class="campoDescripcion" type="text" name="nombre" placeholder="Descripción del departamento" value="">
+                    <input class="campoDescripcion" name="DescDepartamento" type="text" placeholder="Descripción del departamento" value="<?php 
+                        //Devuelve el campo campoDescDepartamento si se había introducido correctamente
+                        if(isset($_REQUEST['DescDepartamento']) && $aErrores["DescDepartamento"] == null){
+                            echo $_REQUEST['DescDepartamento'];
+                        }
+                    ?>"/>
+
+                    <span style="color:red">
+                        <?php
+                        //Imprime el error en el caso de que se introduzca mal el código del Departamento
+                        if($aErrores["DescDepartamento"] != null){
+                            echo $aErrores['DescDepartamento'];
+                        }
+                        ?> 
+                    </span>
                     
                     <input class="botonBuscar" type="submit" value="BUSCAR" name="buscar"/>
                 </form>
             </div>
             
+            <table class="tHead">
+                <tr>
+                    <th>CodDepartamento</th>
+                    <th>DescDepartamento</th>
+                    <th>FechaBaja</th>
+                    <th>VolumenNegocio</th>
+                    <th>Operaciones</th>
+                </tr>
+            </table>
+                    
             <div class="mostrarDepartamentos">
+                <?php
+                    if(isset($_REQUEST['buscar'])){
+                        //Comprobar que el campo campoDescDepartamento se ha rellenado con un alfanumérico
+                        $aErrores["DescDepartamento"] = validacionFormularios::comprobarAlfaNumerico($_REQUEST['DescDepartamento'], 255, 1, 0);
+
+                        //Comprobar si algún campo del array de errores ha sido rellenado
+                        foreach ($aErrores as $clave => $valor) {
+                            //Comprobar si el campo ha sido rellenado
+                            if($valor!=null){
+                                $_REQUEST[$clave] = "";
+                                $entradaOK = false;
+                            }
+                        }
+
+                    }
+                    else{
+                        $entradaOK = false;
+                    }
+                    
+                    if($entradaOK){
+                    //Si los datos han sido introducidos correctamente
+                    $aRespuestas = ["DescDepartamento" => $_REQUEST['DescDepartamento']];
+
+                    //Mostrar registros de la tabla Departamento
+                    try {
+                        //Instanciar un objeto PDO y establecer la conexión con la base de datos
+                        $miDB = new PDO(DSN, USER, PASSWORD);
+
+                        //Establecer PDO::ERRMODE_EXCEPTION como valor del atributo PDO::ATTR_ERRMODE
+                        $miDB->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+                        //Creación de una variable que almacena una consulta sql para insertar los valores en la tabla Departamento
+                        $sql = "SELECT * FROM Departamento WHERE DescDepartamento LIKE '%{$aRespuestas['DescDepartamento']}%'";
+
+                        //Preparación de la consulta
+                        $consulta = $miDB->prepare($sql);
+
+                        //Ejecución de la consulta
+                        $consulta->execute();
+                ?>
                 <table>
-                    <thead>
-                        <tr>
-                            <th>CodDepartamento</th>
-                            <th>DescDepartamento</th>
-                            <th>FechaBaja</th>
-                            <th>VolumenNegocio</th>
-                            <th>Operaciones</th>
-                        </tr>
-                    </thead>
+                    
                     <tbody>
-                        <tr>
-                            <td>Campo</td>
-                            <td>Campo</td>
-                            <td>Campo</td>
-                            <td>Campo</td>
-                            <td>Campo</td>
-                        </tr>
+                    <?php  
+                        $registro = $consulta->fetchObject();
+                        while ($registro != null) {
+                    ?>
+                            <tr>
+                                <td><?php echo $registro->CodDepartamento; ?></td>
+                                <td><?php echo $registro->DescDepartamento; ?></td>
+                                <td><?php echo $registro->FechaBaja; ?></td>
+                                <td><?php echo $registro->VolumenNegocio; ?></td>
+                                <td><?php echo "Operaciones" ?></td>
+                            </tr>
+                            <?php
+                            $registro = $consulta->fetchObject();
+                        }
+                    ?>
                     </tbody>
                 </table>
+                
+                <?php
+                } catch (PDOException $pdoe) {
+                    //Mostrar mensaje de error
+                    echo "<p style='color:red'>ERROR: " . $pdoe . "</p>";
+                } finally {
+                    //Cerrar la conexión
+                    unset($miDB);
+                }
+        }
+                ?>
             </div>
             
             <div class="botBar">
